60b2f4e32616d0396e113e9a36f8d5125920fa0b
Андрій Петруняк <hasherama8@gmail.com>
2022-08-18 10:31:33 +0300

    unit tests with jest

---
+++ tests/ram.test.js
@@ -0,0 +1,32 @@
+const { handlerRAM } = require('../src/RAM/handler.ram');
+const os = require('os');
+
+describe('RAM', () => {
+
+    let result;
+
+    beforeAll(async () => {
+        result = await handlerRAM();
+    })
+
+    test('should be defined', () => {
+        expect(result).toBeDefined();
+    })
+
+    test('should be object', () => {
+        expect(typeof(result)).toBe('object');
+    })
+
+    test('should has property freeMemory', () => {
+        expect(result.hasOwnProperty('freeMemory')).toEqual(true)
+    })
+
+    test('free memory should be grater than 0', () => {
+        expect(result.freeMemory).toBeGreaterThan(0)
+    })
+
+    test('free memory should be less than total memory', () => {
+        expect(result.freeMemory).toBeLessThan(os.totalmem())
+    })
+
+})
