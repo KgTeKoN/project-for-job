01297b3685d556401c75823669e270f7a9b2790c
Андрій Петруняк <hasherama8@gmail.com>
2022-09-04 09:54:19 +0300

    trial version db with migrations

--- src/knex/knexfile.js
+++ src/knex/knexfile.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
 
 module.exports = {
-    client: 'pg',
-    connection: {
-        host: process.env.PG_CONNECTION_HOST,
-        port: process.env.PG_CONNECTION_PORT,
-        user: process.env.PG_CONNECTION_HOST,
-        password: process.env.PG_CONNECTION_PASSWORD,
-        database: process.env.PG_CONNECTION_DB
+    development: {
+        client: 'pg',
+        connection: {
+            host: process.env.PG_CONNECTION_HOST,
+            port: process.env.PG_CONNECTION_PORT,
+            user: process.env.PG_CONNECTION_HOST,
+            password: process.env.PG_CONNECTION_PASSWORD,
+            database: process.env.PG_CONNECTION_DB
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        },
     },
+    onUpdateTrigger: table => `
+    CREATE TRIGGER ${table}_updated_at
+    BEFORE UPDATE ON ${table}
+    FOR EACH ROW
+    EXECUTE PROCEDURE on_update_timestamp();
+  `
 };
---
+++ src/knex/migrations/20220902202710_users_data.js
@@ -0,0 +1,16 @@
+const { onUpdateTrigger } = require('../knexfile')
+
+exports.up = function(knex) {
+  return knex.schema.createTable("users", t => {
+      t.increments("id").primary();
+      t.string("name", 20);
+      t.string("email", 30).notNullable().unique();
+      t.string("password", 20);
+      t.boolean("is_deleted");
+      t.timestamps(true, true);
+  }).then(() => knex.raw(onUpdateTrigger('users')))
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("users");
+};
