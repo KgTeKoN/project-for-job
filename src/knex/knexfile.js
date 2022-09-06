2216e492b9672500cd293b18f536978e5c2be57b
Андрій Петруняк <hasherama8@gmail.com>
2022-09-06 22:25:50 +0300

    migration is finished

--- src/knex/knexfile.js
+++ src/knex/knexfile.js
@@ -4,24 +4,37 @@ module.exports = {
     development: {
         client: 'pg',
         connection: {
-            host: process.env.PG_CONNECTION_HOST,
-            port: process.env.PG_CONNECTION_PORT,
-            user: process.env.PG_CONNECTION_HOST,
-            password: process.env.PG_CONNECTION_PASSWORD,
-            database: process.env.PG_CONNECTION_DB
+            host: process.env.PG_CONNECTION_HOST || 'localhost',
+            port: process.env.PG_CONNECTION_PORT || 5080,
+            user: process.env.PG_CONNECTION_HOST || 'postgres',
+            password: process.env.PG_CONNECTION_PASSWORD || 'example',
+            database: process.env.PG_CONNECTION_DB || 'postgres_db_project_for_job'
         },
         pool: {
-            min: 2,
+            min: 0,
             max: 10
         },
         migrations: {
             tableName: 'knex_migrations'
         },
     },
+    on_updated_timestamp: () =>`
+        CREATE OR REPLACE FUNCTION on_update_timestamp()
+        RETURNS trigger AS $$
+            BEGIN
+                NEW.updated_at = now();
+                RETURN NEW;
+            END;
+        $$ language 'plpgsql';
+    `,
+
+    DROP_ON_UPDATE_TIMESTAMP_FUNCTION: `DROP FUNCTION on_update_timestamp`,
+
     onUpdateTrigger: table => `
-    CREATE TRIGGER ${table}_updated_at
-    BEFORE UPDATE ON ${table}
-    FOR EACH ROW
-    EXECUTE PROCEDURE on_update_timestamp();
-  `
+        CREATE TRIGGER ${table}_updated_at
+        BEFORE UPDATE ON ${table}
+        FOR EACH ROW
+        EXECUTE PROCEDURE on_update_timestamp();
+    `,
+
 };
--- src/knex/migrations/20220902202710_users_data.js
+++ src/knex/migrations/20220902202710_users_data.js
@@ -1,4 +1,4 @@
-const { onUpdateTrigger } = require('../knexfile')
+const { onUpdateTrigger, on_updated_timestamp } = require('../knexfile')
 
 exports.up = function(knex) {
   return knex.schema.createTable("users", t => {
@@ -8,7 +8,7 @@ exports.up = function(knex) {
       t.string("password", 20);
       t.boolean("is_deleted");
       t.timestamps(true, true);
-  }).then(() => knex.raw(onUpdateTrigger('users')))
+  }).then(() => knex.raw(on_updated_timestamp())).then(() => knex.raw(onUpdateTrigger('users')))
 };
 
 exports.down = function(knex) {
