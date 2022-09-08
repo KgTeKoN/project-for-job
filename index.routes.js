db2bf71441ac9c0faf28fa35ca0396396efd1cd5
Андрій Петруняк <hasherama8@gmail.com>
2022-09-08 23:53:33 +0300

    create route.signUp

--- index.routes.js
+++ index.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const RAM = require('./src/RAM/route.ram');
+const auth = require('./src/registration/route.signUp')
 
-routes.use('/api', RAM);
+routes.use('/api', RAM, auth);
 
 module.exports = routes;
---
+++ src/registration/route.signUp.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.post('v1/auth/sign-up');
+
+module.exports = routes
