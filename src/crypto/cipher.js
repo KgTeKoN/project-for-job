161ba807297ba8b716f8b6b3097ebe7ee5b26e8a
Андрій Петруняк <hasherama8@gmail.com>
2022-09-22 15:04:49 +0300

    change readme.md

--- src/crypto/cipher.js
+++ src/crypto/cipher.js
@@ -2,9 +2,7 @@ const crypto = require('crypto');
 const { cipherAlgorithm } = require('../../config');
 require('dotenv').config();
 
-const secret = process.env.SECRET_KEY;
-const key = crypto.createHash('sha256').update(String(secret))
-     .digest('hex').substr(0, 32);
+const key = Buffer.from(process.env.SECRET_KEY, 'hex');
 
 const encryptionPassword = async (password) => {
     const iv = crypto.randomBytes(16).toString('hex').slice(0, 16);;
