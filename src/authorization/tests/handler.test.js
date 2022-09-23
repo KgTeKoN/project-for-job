b58687248a0335d7f940777148bc1d2f997bbb27
Андрій Петруняк <hasherama8@gmail.com>
2022-09-24 01:28:15 +0300

    finished unit tests (test for handler.addUser)

--- src/authorization/tests/handler.test.js
+++ src/authorization/tests/handler.test.js
@@ -1,24 +1,59 @@
 const { createHash, encryptData } = require('../../crypto/crypto');
 const { addUserInDB } = require('../handler.addUserInDB')
-let data = {
+const PersonController = require('../../PersonCRUD/person.controller')
+
+const data = {
     name: "Andriy",
     email: "hasherama8@gmail.com",
     password: "password"
 }
+const testPasswordCipher = '123456';
+const testPasswordHash = 'qwerty'
 
-describe('validate authorization handler', () => {
-    test('validate returned data from createHash', async () => {
-        jest.doMock(addUserInDB, async () => {
-            return jest.fn(async (data) => {
-                const { name, email, password } = data;
-                const hash = await createHash(password);
-                const encryptedPassword = await encryptData(hash);
+jest.mock('../../crypto/crypto', () => {
+    const originalModule = jest.requireActual('../../crypto/crypto');
 
-            })
-            await addUserInDB(data)
-            expect(createHash).toHaveBeenCalledWith(data.password)
-        })
+    return {
+        __esModule: true,
+        ...originalModule,
+        createHash: jest.fn(() => testPasswordCipher),
+        encryptData: jest.fn(() => testPasswordHash)
+    };
+});
 
-    })
+jest.mock('../../PersonCRUD/person.controller', () => {
+    const originalModule = jest.requireActual('../../PersonCRUD/person.controller');
+
+    return {
+        __esModule: true,
+        ...originalModule,
+        createPerson: jest.fn(() => 'test add in DB was successful')
+    };
+});
+
+beforeAll(async () => {
+    await addUserInDB(data)
 })
 
+describe('test handler', () => {
+
+    test('input createHash data must be as data.password', () => {
+        expect(createHash).toBeCalledWith(data.password);
+        expect(createHash).toBeCalledTimes(1);
+    })
+
+    test('input encryptData data must be as testPassword', () => {
+        expect(encryptData).toBeCalledWith(testPasswordCipher);
+        expect(encryptData).toBeCalledTimes(1);
+    })
+
+    test('input PersonController data must be object', () => {
+        expect(PersonController.createPerson).toBeCalledWith(
+            data.name,
+            data.email,
+            testPasswordHash
+        );
+        expect(PersonController.createPerson).toBeCalledTimes(1)
+    })
+
+})
